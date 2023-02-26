6dc339096968b6085ee126ffe06b19ded0d4eeb3
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-02-26 14:18:26 -0600

    location

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { Form } from "react-bootstrap";
+import $ from 'jquery';
 
 const Home = () => {
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        });
+  
+        await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+      }
+      fetchData();
+    }, [lat,long])
+
     return (
         <div className="Home">
                     <Form className="SearchBarPosition" >
@@ -14,7 +36,7 @@ const Home = () => {
           />
       </Form>
         <div className="main-weather">
-            <h1> Todays Weather</h1>
+            <h1 className="today"> Todays Weather</h1>
             </div>
         </div>
     );
